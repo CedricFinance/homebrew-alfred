208c113670a855403902713a6584c95aad8cb51e
Cedric Finance <cedric.finance@finandric.com>
2022-07-12 10:50:53 +0200

    Fix alfred-epoch-converter url

--- Casks/alfred-epoch-converter.rb
+++ Casks/alfred-epoch-converter.rb
@@ -2,7 +2,7 @@ cask "alfred-epoch-converter" do
   version "1.0.3"
   sha256 "8b9db56ff7fd13112764f4b4d68d62fd1d8b7b12cecc271b5c215b62728fd661"
 
-  url "https://github.com/snooze92/alfred-epoch-converter/releases/download/v#{version}/epoch-converter-#{version.tr(".", "_")}.alfredworkflow"
+  url "https://github.com/snooze92/alfred-epoch-converter/releases/download/#{version}/epoch-converter-#{version.tr(".", "_")}.alfredworkflow"
   appcast "https://github.com/snooze92/alfred-epoch-converter/releases"
   name "alfred-epoch-converter"
   desc "Makes it easy to work with epoch timestamps!"
