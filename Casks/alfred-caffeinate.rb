15cee62419e9dedfdfccc20f7cbb5da9dd205c5d
Cedric Finance <cedric.finance@finandric.com>
2022-07-12 11:19:03 +0200

    Fix alfred-caffeinate url

--- Casks/alfred-caffeinate.rb
+++ Casks/alfred-caffeinate.rb
@@ -2,7 +2,7 @@ cask "alfred-caffeinate" do
   version "1.0.0"
   sha256 "148696a95241ce6e39ac4a615cfd077d7d1f96945f793128b227288079d7c364"
 
-  url "https://github.com/shawnrice/alfred-2-caffeinate-workflow/raw/master/Caffeinate Control.alfredworkflow"
+  url "https://github.com/shawnrice/alfred-2-caffeinate-workflow/raw/master/Caffeinate%20Control.alfredworkflow"
   appcast "https://github.com/shawnrice/alfred-2-caffeinate-workflow/releases"
   name "alfred-caffeinate"
   desc "An Alfred2 workflow to control the system caffeinate utility (prevents sleep)."
